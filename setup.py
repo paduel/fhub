412b64ba20fb642a2fedd09be56a1292f37f5af6
paduel <paduel@gmail.com>
2020-04-29 18:19:02 +0200

    Updated requirements.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fhub',
+    version='0.4',
+    packages=['fhub'],
+    url='https://github.com/paduel/fhub',
+    license='Apache 2.0',
+    author='Paduel',
+    author_email='paduel@gmail.com',
+    description='Python client for Finnhub API '
+)
